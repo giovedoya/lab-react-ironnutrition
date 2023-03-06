b3dd11322c2cee4e9f899fd30f2c461894dca1da
Giovanni Bedoya <giovedoya@gmail.com>
2023-03-06 06:17:51 +0100

    'Iteration 1 | Render a Simple List'

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import foods from "./foods.json";
 import './App.css';
+import AppI from "./App.example";
+import React, { useState } from "react";
+import { Row, Divider, Button } from 'antd';
+
+
+
 
 function App() {
+
+  const [food, setFoods] = useState(foods)
+
   return (
     <div className="App">
-      
+      {/* Display Add Food component here */}
+
+      <Button> Hide Form / Add New Food </Button>
+
+      {/* Display Search component here */}
+
+      <Divider>Food List</Divider>
+
+      <Row style={{ width: '100%', justifyContent: 'center' }}>
+      {food.map((elem, i) => {
+        return (
+        <div key={i}>
+          <p>{elem.name}</p>
+          <img src={elem.image} alt={elem.name} width={100} />
+          </div>
+          )
+      })}
+      </Row>
     </div>
   );
 }
